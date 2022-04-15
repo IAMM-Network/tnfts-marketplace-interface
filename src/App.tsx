ed408419647f6a9d51e6716468835aa260f72aa9
garvazsof <garvazsof@gmail.com>
2022-04-15 12:36:00 -0500

    Initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="header">
+          <span>header</span>
+        </div>
+        <div className="appBody">
+          <span>Body</span>
+        </div>
+        <div className="footer">
+          <span>Footer</span>
+        </div>
+      </div>
     </div>
   );
 }
