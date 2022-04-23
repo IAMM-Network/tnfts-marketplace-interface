edf99c733634f984306b716f8219cd5f360f006d
luisantoniocrag <tricubeart@gmail.com>
2022-04-23 16:00:49 -0500

    feature/Box components

---
+++ src/Components/Box/index.tsx
@@ -0,0 +1,4 @@
+export { default as Box } from "./Box";
+export { default as Flex } from "./Flex";
+export { default as Gtid } from "./Grid";
+export type { BoxProps, FlexProps, GridProps } from "./types";
