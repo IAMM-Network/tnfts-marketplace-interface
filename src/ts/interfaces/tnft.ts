0ce727646ca5680135e4c46e2412a3094fc0fc42
garvazsof <garvazsof@gmail.com>
2022-04-28 20:08:55 -0500

    Adding types for the create new item screen

---
+++ src/ts/interfaces/tnft.ts
@@ -0,0 +1,31 @@
+import StringKeyPair from "../types/stringKeyPair";
+import FileType from "../types/fileType"
+
+interface TNFT{ 
+    name: string;
+    fileType: FileType;
+    file: string;
+    externalLink: string;
+    description: string;
+    howSmart: Smart;
+    impact: Impact;
+    supply: number
+
+ }
+
+ export default TNFT;
+
+
+
+interface Smart{ 
+    ownership: boolean;
+    timelock: boolean;
+    generative: boolean;
+}
+
+interface Impact{ 
+    properties: StringKeyPair[];
+    levels: StringKeyPair[];
+    stats: StringKeyPair[];
+    nsfw: boolean;
+}
