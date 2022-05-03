9c45a7ef8d33557a130a089de69fbebac7507bc9
luisantoniocrag <tricubeart@gmail.com>
2022-05-03 16:49:42 -0500

    change variables names

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -17,11 +17,11 @@ const Button = <E extends ElementType = "button">(props: ButtonProps<E>): JSX.El
   const classNames = className ? [className] : [];
 
   if (isLoading) {
-    classNames.push("pancake-button--loading");
+    classNames.push("iamm-button--loading");
   }
 
   if (isDisabled && !isLoading) {
-    classNames.push("pancake-button--disabled");
+    classNames.push("iamm-button--disabled");
   }
 
   return (
