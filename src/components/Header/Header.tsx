a00b60aa159c047671b72a4f6471e9f53cb00a75
luisantoniocrag <tricubeart@gmail.com>
2022-06-07 12:47:35 -0500

    handle hamburguerMenu condition

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Container } from '../Layout';
 import { HeaderWrapper } from './styles';
 import { DiscordMediaIcon, IAMMTextIcon, TwitterMediaIcon } from '../Svg';
 import { Box, Flex } from '../Box';
+import Hamburger from 'hamburger-react';
+
+const socialMedia = () => (
+  <Flex>
+    <Box marginRight={24}>
+      <a href='https://twitter.com/IAMM_Network' target='_blank' rel='noreferrer'>
+        <TwitterMediaIcon />
+      </a>
+    </Box>
+    <Box>
+      <a href='https://discord.com/invite/jQ4rGjj7fZ' target='_blank' rel='noreferrer'>
+        <DiscordMediaIcon />
+      </a>
+    </Box>
+  </Flex>
+);
+
+const hamburguerMenu = (color: string, toggled: boolean, toggle: React.Dispatch<React.SetStateAction<boolean>>) => (
+  <Flex>
+    <Hamburger color={color} toggled={toggled} toggle={toggle} />
+  </Flex>
+);
 
 const Header: React.FC = () => {
   const { pathname } = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const getRender = (): boolean => pathname !== '/';
 
   return (
-    <HeaderWrapper main={pathname !== '/'}>
+    <HeaderWrapper main={getRender()}>
       <Container maxWidth='90%'>
         <Flex alignItems='center' justifyContent='space-between' width='100%'>
           <IAMMTextIcon width='100px' fill='white' />
-          <Flex>
-            <Box marginRight={24}>
-              <a href='https://twitter.com/IAMM_Network' target='_blank' rel='noreferrer'>
-                <TwitterMediaIcon />
-              </a>
-            </Box>
-            <Box>
-              <a href='https://discord.com/invite/jQ4rGjj7fZ' target='_blank' rel='noreferrer'>
-                <DiscordMediaIcon />
-              </a>
-            </Box>
-          </Flex>
+          {getRender() ? hamburguerMenu('white', isOpen, setIsOpen) : socialMedia()}
         </Flex>
       </Container>
     </HeaderWrapper>
